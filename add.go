0b613cdf589741cd0866a0fcc2b10e79dfb1b8be
Ishraq Hasan <ishraq10199@gmail.com>
2025-10-06 08:24:17 +0600

    #06 Oct 2025: Go: Learning about versioning - ex2

--- add.go
+++ add.go
@@ -1,3 +1,11 @@
+// Package learninggoch10exercise provides an addition utility,
+// made as an exercise on versioning in Go
+//
+// The book for which this exercise was done:
+// [Learning Go by Jon Bodner]: https://www.oreilly.com/library/view/learning-go-2nd/9781098139285
 package learninggoch10exercise
 
+// Add adds two integers and returns the sum
+//
+// More on addition: https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int { return a + b }
